63c1dc5e719b4e056232e17106fbf89a92f1aa77
michael007js <shill@eagersoft.cn>
2025-03-05 18:01:54 +0800

    升级

--- app/src/main/java/com/sss/michael/powermanager/application/MyApplication.java
+++ app/src/main/java/com/sss/michael/powermanager/application/MyApplication.java
@@ -3,6 +3,8 @@ package com.sss.michael.powermanager.application;
 import android.app.Application;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
+import android.content.Context;
+import android.os.PowerManager;
 
 import com.sss.michael.powermanager.constant.AppConstant;
 import com.sss.michael.powermanager.receiver.MyAdmin;
@@ -13,11 +15,14 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (AppConstant.DPM == null){
+        if (AppConstant.DPM == null) {
             AppConstant.DPM = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
         }
-        if (AppConstant.CN == null){
+        if (AppConstant.CN == null) {
             AppConstant.CN = new ComponentName(this, MyAdmin.class);
+        }
+        if (AppConstant.PM == null) {
+            AppConstant.PM = (PowerManager) getSystemService(Context.POWER_SERVICE);
         }
         AppConstant.ROOT = CheckRoot.isDeviceRooted();
     }
--- app/src/main/java/com/sss/michael/powermanager/constant/AppConstant.java
+++ app/src/main/java/com/sss/michael/powermanager/constant/AppConstant.java
@@ -2,12 +2,14 @@ package com.sss.michael.powermanager.constant;
 
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
+import android.os.PowerManager;
 
 public class AppConstant {
     /**
      * 设备策略服务
      */
     public static DevicePolicyManager DPM;
+    public static PowerManager PM;
     public static ComponentName CN;
     public static boolean ROOT;
     public static final int REBOOT_SHUTDOWN = 1;
